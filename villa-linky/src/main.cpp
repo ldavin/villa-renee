7dfed070d8096c13749be3b4becc2c734551249f
Louis Davin <louis@applizi.com>
2022-06-25 12:02:24 +0200

    Use RocketScream for interop, make villa-linky feather compatible

--- villa-linky/src/main.cpp
+++ villa-linky/src/main.cpp
@@ -1,23 +1,40 @@
 #include <Arduino.h>
-#include <RFM69_ATC.h>
-#include <LowPower.h>
-#include <SPIFlash.h>
+#include <RHReliableDatagram.h>
+#include <RH_RF69.h>
 #include <Parser.h>
 
-#define AcquisitionSerial board_specific_serial
 #define startFrame 0x02
 #define endFrame 0x03
 
-#define RadioFrequency RF69_868MHZ
 #define RadioGatewayNodeId 1
 #define RadioNodeId 2
 #define RadioNetworkId 100
 #define RadioKey "sampleEncryptKey"
-#define RadioAtcRssi -75
 
-Parser parser;
-RFM69_ATC radio;
+#ifdef BoardIsMoteino
+
+#include <LowPower.h>
+#include <SPIFlash.h>
+
+#define ReadPin A7
+#define AcquisitionSerial Serial
+
+RH_RF69 driver;
 SPIFlash flash(SS_FLASHMEM, 0xEF30);
+#endif
+
+#ifdef BoardIsFeather
+
+#include <Adafruit_SleepyDog.h>
+
+#define ReadPin A5
+#define AcquisitionSerial Serial1
+
+RH_RF69 driver(8, 7);
+#endif
+
+RHReliableDatagram radio(driver, RadioNodeId);
+Parser parser;
 
 void blinkFor(int durationMillis);
 
@@ -25,31 +42,33 @@ void sendData(unsigned long whReading, unsigned long apparentPower);
 
 bool superCapacitorIsChargedEnough();
 
+void powerDown();
+
+void disableFlashChip();
+
 void setup() {
     pinMode(LED_BUILTIN, OUTPUT);
     AcquisitionSerial.begin(1200);
-    pinMode(A7, INPUT);
+    pinMode(ReadPin, INPUT);
 
-    radio.initialize(RadioFrequency, RadioNodeId, RadioNetworkId);
-    radio.setHighPower();
-    radio.encrypt(RadioKey);
-    radio.enableAutoPower(RadioAtcRssi);
-    radio.sleep();
+    radio.init();
+    driver.setFrequency(868.0);
+    driver.setTxPower(5, true);
+    driver.setEncryptionKey(); // TODO
+    driver.sleep();
 
-    if (flash.initialize()) {
-        flash.sleep();
-    }
+    disableFlashChip();
 
     delay(1000);
 
-    for (int i = 0; i < 3; ++i) {
-        blinkFor(100);
+    for (int i = 0; i < 10; ++i) {
+        blinkFor(20);
     }
 }
 
 void loop() {
     if (!superCapacitorIsChargedEnough()) {
-        LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
+        powerDown();
         blinkFor(1000);
         return;
     }
@@ -75,18 +94,18 @@ void loop() {
         blinkFor(100);
         sendData(whReading, apparentPower);
     } else {
-        parser.reset();
         blinkFor(20);
         blinkFor(20);
         blinkFor(20);
     }
+    parser.reset();
 
     // End of complete frame capture
     blinkFor(100);
 }
 
 bool superCapacitorIsChargedEnough() {
-    int read = analogRead(A7);
+    int read = analogRead(ReadPin);
     // Input of a voltage divider bridge on a 3.3V board
     float voltage = read * 2 * 3.3f / 1023;
     return voltage >= 3.7;
@@ -94,7 +113,32 @@ bool superCapacitorIsChargedEnough() {
 
 void sendData(unsigned long whReading, unsigned long apparentPower) {
     // TODO Come up with a format for the radio gateway and really send the data
-    radio.sendWithRetry(RadioGatewayNodeId, "Hi", 2);
+    uint8_t data[] = "Hi!";
+    if (radio.sendtoWait(data, sizeof(data), RadioGatewayNodeId)) {
+        blinkFor(500);
+    } else {
+        for (int i = 0; i < 3; ++i) {
+            blinkFor(100);
+        }
+    }
+    driver.sleep();
+}
+
+void disableFlashChip() {
+#ifdef BoardIsMoteino
+    if (flash.initialize()) {
+        flash.sleep();
+    }
+#endif
+}
+
+void powerDown() {
+#ifdef BoardIsMoteino
+    LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
+#endif
+#ifdef BoardIsFeather
+    Watchdog.sleep(8000);
+#endif
 }
 
 void blinkFor(int durationMillis) {
