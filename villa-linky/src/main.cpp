9e64ceb0a47e6f322566aa656ff91488e96de348
Louis Davin <louis@applizi.com>
2022-06-25 10:32:52 +0200

    Moteino specific test for linky acquisition circuit

--- villa-linky/src/main.cpp
+++ villa-linky/src/main.cpp
@@ -1,23 +1,59 @@
 #include <Arduino.h>
+#include <RFM69_ATC.h>
+#include <LowPower.h>
+#include <SPIFlash.h>
+#include <Parser.h>
 
 #define AcquisitionSerial board_specific_serial
 #define startFrame 0x02
 #define endFrame 0x03
 
+#define RadioFrequency RF69_868MHZ
+#define RadioGatewayNodeId 1
+#define RadioNodeId 2
+#define RadioNetworkId 100
+#define RadioKey "sampleEncryptKey"
+#define RadioAtcRssi -75
+
+Parser parser;
+RFM69_ATC radio;
+SPIFlash flash(SS_FLASHMEM, 0xEF30);
+
 void blinkFor(int durationMillis);
+
+void sendData(unsigned long whReading, unsigned long apparentPower);
+
+bool superCapacitorIsChargedEnough();
 
 void setup() {
     pinMode(LED_BUILTIN, OUTPUT);
     AcquisitionSerial.begin(1200);
+    pinMode(A7, INPUT);
+
+    radio.initialize(RadioFrequency, RadioNodeId, RadioNetworkId);
+    radio.setHighPower();
+    radio.encrypt(RadioKey);
+    radio.enableAutoPower(RadioAtcRssi);
+    radio.sleep();
+
+    if (flash.initialize()) {
+        flash.sleep();
+    }
+
     delay(1000);
 
     for (int i = 0; i < 3; ++i) {
         blinkFor(100);
-        delay(100);
     }
 }
 
 void loop() {
+    if (!superCapacitorIsChargedEnough()) {
+        LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
+        blinkFor(1000);
+        return;
+    }
+
     char charIn = 0;
 
     while (charIn != startFrame) {
@@ -29,17 +65,41 @@ void loop() {
     while (charIn != endFrame) {
         if (AcquisitionSerial.available()) {
             charIn = AcquisitionSerial.read() & 0x7F;
-
-            // TODO Do something with the char
+            parser.feed(charIn);
         }
     }
 
+    if (parser.dataIsAvailable()) {
+        unsigned long whReading = parser.getWhReading();
+        unsigned long apparentPower = parser.getApparentPower();
+        blinkFor(100);
+        sendData(whReading, apparentPower);
+    } else {
+        parser.reset();
+        blinkFor(20);
+        blinkFor(20);
+        blinkFor(20);
+    }
+
     // End of complete frame capture
-    blinkFor(200);
+    blinkFor(100);
+}
+
+bool superCapacitorIsChargedEnough() {
+    int read = analogRead(A7);
+    // Input of a voltage divider bridge on a 3.3V board
+    float voltage = read * 2 * 3.3f / 1023;
+    return voltage >= 3.7;
+}
+
+void sendData(unsigned long whReading, unsigned long apparentPower) {
+    // TODO Come up with a format for the radio gateway and really send the data
+    radio.sendWithRetry(RadioGatewayNodeId, "Hi", 2);
 }
 
 void blinkFor(int durationMillis) {
     digitalWrite(LED_BUILTIN, HIGH);
     delay(durationMillis);
     digitalWrite(LED_BUILTIN, LOW);
+    delay(durationMillis);
 }
