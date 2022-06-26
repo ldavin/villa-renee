09d5e4266722bc9133e15f3623b95e9752405b5b
Louis Davin <louis@applizi.com>
2022-06-26 17:58:12 +0200

    Add a sample encryptionKey file

---
+++ villa-linky/src/encryption_key.sample.h
@@ -0,0 +1,14 @@
+//
+// Created by Louis Davin on 26/06/2022.
+// Duplicate, rename this file to encryption_key.h, and customize the network encryption key.
+//
+
+#include <stdint.h>
+
+#ifndef VILLALINKY_ENCRYPTION_KEY_H
+#define VILLALINKY_ENCRYPTION_KEY_H
+
+uint8_t encryptionKey[] = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
+                           0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
+
+#endif //VILLALINKY_ENCRYPTION_KEY_H
