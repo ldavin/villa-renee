a9cddb74e4a01aa56cfb0afc55c7dda43d57c21d
Louis Davin <louis@applizi.com>
2022-07-03 20:24:06 +0200

    Linky: Send real data, take voltage value 3 times and average

--- villa-linky/src/main.cpp
+++ villa-linky/src/main.cpp
@@ -9,8 +9,6 @@
 
 #define RadioGatewayNodeId 1
 #define RadioNodeId 2
-#define RadioNetworkId 100
-#define RadioKey "sampleEncryptKey"
 
 #ifdef BoardIsMoteino
 
@@ -36,6 +34,8 @@ RH_RF69 driver(8, 7);
 
 RHReliableDatagram radio(driver, RadioNodeId);
 Parser parser;
+long lowChargeDelaysCount = 0;
+long sendErrorsCount = 0;
 
 void blinkFor(int durationMillis);
 
@@ -47,6 +47,8 @@ void powerDown();
 
 void disableFlashChip();
 
+void flushSerial();
+
 void setup() {
     pinMode(LED_BUILTIN, OUTPUT);
     AcquisitionSerial.begin(1200);
@@ -54,7 +56,7 @@ void setup() {
 
     radio.init();
     driver.setFrequency(868.0);
-    driver.setTxPower(5, true);
+    driver.setTxPower(-2, true);
     driver.setEncryptionKey(encryptionKey);
     driver.sleep();
 
@@ -69,8 +71,10 @@ void setup() {
 
 void loop() {
     if (!superCapacitorIsChargedEnough()) {
+        lowChargeDelaysCount++;
         powerDown();
         blinkFor(1000);
+        flushSerial();
         return;
     }
 
@@ -92,34 +96,49 @@ void loop() {
     if (parser.dataIsAvailable()) {
         unsigned long whReading = parser.getWhReading();
         unsigned long apparentPower = parser.getApparentPower();
-        blinkFor(100);
         sendData(whReading, apparentPower);
-    } else {
-        blinkFor(20);
-        blinkFor(20);
-        blinkFor(20);
+        flushSerial();
     }
     parser.reset();
 
     // End of complete frame capture
-    blinkFor(100);
 }
 
 bool superCapacitorIsChargedEnough() {
-    int read = analogRead(ReadPin);
+    int read1 = analogRead(ReadPin);
+    delay(1);
+    int read2 = analogRead(ReadPin);
+    delay(1);
+    int read3 = analogRead(ReadPin);
+    int read = (read1 + read2 + read3) / 3;
     // Input of a voltage divider bridge on a 3.3V board
     float voltage = read * 2 * 3.3f / 1023;
     return voltage >= 3.7;
 }
 
 void sendData(unsigned long whReading, unsigned long apparentPower) {
-    // TODO Come up with a format for the radio gateway and really send the data
-    uint8_t data[] = "Hi!";
-    if (radio.sendtoWait(data, sizeof(data), RadioGatewayNodeId)) {
-        blinkFor(500);
+    String dataString;
+    dataString.reserve(20);
+    dataString.concat(whReading);
+    dataString.concat(",");
+    dataString.concat(apparentPower);
+    dataString.concat(",");
+    dataString.concat(lowChargeDelaysCount);
+    dataString.concat(",");
+    dataString.concat(sendErrorsCount);
+
+    char data[dataString.length()];
+    dataString.toCharArray(data, sizeof(data));
+
+    if (radio.sendtoWait((uint8_t *) data, sizeof(data), RadioGatewayNodeId)) {
+        lowChargeDelaysCount = 0;
+        sendErrorsCount = 0;
+        blinkFor(50);
     } else {
+        sendErrorsCount++;
+        Serial.println("Message not received :(");
         for (int i = 0; i < 3; ++i) {
-            blinkFor(100);
+            blinkFor(20);
         }
     }
     driver.sleep();
@@ -147,4 +166,10 @@ void blinkFor(int durationMillis) {
     delay(durationMillis);
     digitalWrite(LED_BUILTIN, LOW);
     delay(durationMillis);
+}
+
+void flushSerial() {
+    while (Serial.available() > 0) {
+        Serial.read();
+    }
 }
