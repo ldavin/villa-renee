a1f26bad9acc7a781f19c81a9712daf770294a7d
Louis Davin <louis@applizi.com>
2022-08-28 15:12:03 +0200

    Gazpar: Init basic project based on Linky radio logic and LowPower interrupt sample

---
+++ villa-gazpar/README.md
@@ -0,0 +1,7 @@
+# Villa-Gazpar
+
+This project contains the source code for a microcontroller board plugged to the pulse output of a Gazpar (French connected gas meter).
+The readings are then sent through radio.
+The project uses platform.io and can target
+the [Moteino](https://lowpowerlab.com/guide/moteino/)
+and [Adafruit Feather](https://learn.adafruit.com/adafruit-feather-32u4-radio-with-rfm69hcw-module/overview) boards.
---
+++ villa-gazpar/src/main.cpp
@@ -0,0 +1,144 @@
+#include <Arduino.h>
+#include <LowPower.h>
+#include <RHReliableDatagram.h>
+#include <RH_RF69.h>
+#include "encryption_key.h"
+
+#define RadioGatewayNodeId 1
+#define RadioNodeId 3
+#define RadioMinPower -2
+#define RadioMaxPower 10
+
+
+#ifdef BoardIsMoteino
+
+#include <SPIFlash.h>
+
+#define InterruptPin PIND3
+
+RH_RF69 driver;
+SPIFlash flash(SS_FLASHMEM, 0xEF30);
+#endif
+
+#ifdef BoardIsFeather
+
+#define InterruptPin PIND0
+
+RH_RF69 driver(8, 7);
+#endif
+
+RHReliableDatagram radio(driver, RadioNodeId);
+volatile long pulseReceived = 0;
+long sendErrorsCount = 0;
+long sendErrorsCountAtCurrentRadioPower = 0;
+long sendSuccessCountAtCurrentRadioPower = 0;
+int radioPower = RadioMinPower;
+
+void blinkFor(int durationMillis);
+
+void readPulse();
+
+void disableFlashChip();
+
+void sendData();
+
+void setup() {
+    pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(InterruptPin, INPUT_PULLUP);
+
+    radio.init();
+    radio.setRetries(5);
+    driver.setFrequency(868.0);
+    driver.setTxPower(radioPower, true);
+    driver.setEncryptionKey(encryptionKey);
+    driver.sleep();
+
+    disableFlashChip();
+
+
+    for (int i = 0; i < 10; ++i) {
+        blinkFor(20);
+    }
+}
+
+void loop() {
+    int interruptNum = digitalPinToInterrupt(InterruptPin);
+    attachInterrupt(interruptNum, readPulse, RISING);
+    LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
+
+    detachInterrupt(interruptNum);
+
+    if (pulseReceived > 0) {
+        sendData();
+    }
+}
+
+void readPulse() {
+    pulseReceived++;
+}
+
+void sendData() {
+    String dataString;
+    dataString.reserve(10);
+    dataString.concat(pulseReceived);
+    dataString.concat(",");
+    dataString.concat(sendErrorsCount);
+    dataString.concat(",");
+    dataString.concat(radioPower);
+    dataString.concat(".");
+
+    char data[dataString.length()];
+    dataString.toCharArray(data, sizeof(data));
+
+    if (radio.sendtoWait((uint8_t *) data, sizeof(data), RadioGatewayNodeId)) {
+        sendErrorsCount = 0;
+        blinkFor(50);
+
+        sendSuccessCountAtCurrentRadioPower++;
+        if (sendSuccessCountAtCurrentRadioPower >= 50) {
+            sendSuccessCountAtCurrentRadioPower = 0;
+            sendErrorsCountAtCurrentRadioPower = 0;
+            if (radioPower > RadioMinPower) {
+                radioPower--;
+                driver.setTxPower(radioPower, true);
+                for (int i = 0; i < 3; ++i) {
+                    blinkFor(20);
+                }
+            }
+        }
+    } else {
+        sendErrorsCount++;
+        for (int i = 0; i < 3; ++i) {
+            blinkFor(20);
+        }
+
+        sendErrorsCountAtCurrentRadioPower++;
+        sendSuccessCountAtCurrentRadioPower = 0;
+        if (sendErrorsCountAtCurrentRadioPower >= 5) {
+            sendErrorsCountAtCurrentRadioPower = 0;
+            if (radioPower < RadioMaxPower) {
+                radioPower++;
+                driver.setTxPower(radioPower, true);
+                for (int i = 0; i < 3; ++i) {
+                    blinkFor(20);
+                }
+            }
+        }
+    }
+    driver.sleep();
+}
+
+void disableFlashChip() {
+#ifdef BoardIsMoteino
+    if (flash.initialize()) {
+        flash.sleep();
+    }
+#endif
+}
+
+void blinkFor(int durationMillis) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(durationMillis);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(durationMillis);
+}
